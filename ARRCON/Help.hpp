86403cda822f12883ed42c2f8f291df116564607
radj307 <radj307@gmail.com>
2022-01-05 13:17:22 -0500

    Fix help doc tabs

--- ARRCON/Help.hpp
+++ ARRCON/Help.hpp
@@ -14,8 +14,8 @@ struct Help {
 private:
 	const std::string _program_name;
 	const std::vector<std::pair<std::string, std::string>> _options{
-		{ "-H <Host>  --host <Host>"s, "RCON Server Address or saved hostname.	(Default: \""s + Global.DEFAULT_TARGET.hostname + "\")"s },
-		{ "-P <Port>  --port <Port>"s, "RCON Server Port.						(Default: \""s + Global.DEFAULT_TARGET.port + "\")"s },
+		{ "-H <Host>  --host <Host>"s, "RCON Server Address or saved hostname.  (Default: \""s + Global.DEFAULT_TARGET.hostname + "\")"s },
+		{ "-P <Port>  --port <Port>"s, "RCON Server Port.                       (Default: \""s + Global.DEFAULT_TARGET.port + "\")"s },
 		{ "-p <Password>  --pass <Pass>"s, "RCON Server Password." },
 		{ "-f <file>  --file <file>"s, "Load the specified file and run each line as a command."s },
 		{ "-h  --help"s, "Show the help display."s },
