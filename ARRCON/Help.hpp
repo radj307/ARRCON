404050473a914a285bd1cb1058be35a84742c68d
radj307 <radj307@gmail.com>
2022-01-23 15:11:27 -0500

    fix this for the 3rd time, thanks git

--- ARRCON/Help.hpp
+++ ARRCON/Help.hpp
@@ -13,13 +13,11 @@
 struct Help {
 private:
 	const std::filesystem::path _program_name;
-	const size_t _longest_optname, _max_line_length;
 
 public:
-	Help(const std::filesystem::path& program_name, const size_t& max_line_sz = 120ull) : _program_name{ program_name }, _longest_optname{ [this]() { size_t longest{0ull}; for (auto& [optname, desc] : _options) if (const auto sz{optname.size()}; sz > longest) longest = sz; return longest; }() }, _max_line_length{ max_line_sz } {}
+	Help(const std::filesystem::path& program_name) : _program_name{ program_name } {}
 	friend std::ostream& operator<<(std::ostream& os, const Help& help)
 	{
-		const auto tabSize{ help._longest_optname + 2ull };
 		os << std::filesystem::path(help._program_name).replace_extension().generic_string() << ' ' << ((help._program_name != std::string_view(DEFAULT_PROGRAM_NAME)) ? "("s + DEFAULT_PROGRAM_NAME + ") "s : "") << "v" << ARRCON_VERSION << '\n'
 			<< "Commandline Remote-CONsole Client.\n"
 			<< '\n'
